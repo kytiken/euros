6fd0bc773710288fa2c2803efe7f8d3e12a20489
kytiken <kytiken@gmail.com>
2017-12-15 19:51:43 +0900

    Setting project for documentation, lint and test

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,12 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+if Mix.env == :dev do
+  config :mix_test_watch,
+    clear: true,
+    tasks: ["test", "credo"]
+end
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
--- mix.exs
+++ mix.exs
@@ -7,8 +7,15 @@ defmodule Euros.Mixfile do
       version: "0.0.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
-    ]
+      deps: deps(),
+
+      # Docs
+      name: "Euros",
+      source_url: "https://github.com/kytiken/euros",
+      homepage_url: "https://github.com/kytiken/euros",
+      docs: [main: "Euros", # The main page in the docs
+            extras: ["README.md"]]
+      ]
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -21,8 +28,11 @@ defmodule Euros.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:mix_test_watch, "~> 0.5", only: :dev, runtime: false},
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      {:httpoison, "~> 0.13"},
+      {:floki, "~> 0.19.0"}
     ]
   end
 end
