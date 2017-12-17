dc26cc40b5eed33fe952ee299c3867df74f2ee5e
kytiken <kytiken@gmail.com>
2017-12-17 14:07:06 +0900

    Add test pattern for fetch_pages function

--- test/euros/http_test.exs
+++ test/euros/http_test.exs
@@ -4,6 +4,9 @@ defmodule Euros.HTTPTest do
   describe "Euros.HTTP.fetch_pages/1" do
     test "get a web page" do
       assert %{body: _, headers: _, request_url: _, status_code: 200} = Euros.HTTP.fetch_pages("http://localhost:32768")
+      assert %{body: _, headers: _, request_url: _, status_code: 200} = Euros.HTTP.fetch_pages("http://localhost:32768/index.html")
+      assert %{body: _, headers: _, request_url: _, status_code: 200} = Euros.HTTP.fetch_pages("http://localhost:32768/about.html")
+      assert %{body: _, headers: _, request_url: _, status_code: 200} = Euros.HTTP.fetch_pages("http://localhost:32768/nestedpath/index.html")
     end
   end
 end
