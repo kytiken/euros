639eff1f12f8fe438d2ea78a021739493647cb8d
kytiken <kytiken@gmail.com>
2017-12-22 14:42:30 +0900

    Fixed the function filter links for only html page link from the list

--- lib/euros/page.ex
+++ lib/euros/page.ex
@@ -16,18 +16,31 @@ defmodule Euros.Page do
   """
   def link_uris(%HTTPoison.Response{} = response) do
     base_uri = to_base_uri(response)
-    uris = all_link_uris(response)
-    Enum.filter(uris, fn(uri) -> Euros.URI.is_same_host(uri, base_uri) end)
+    response
+    |> all_link_uris
+    |> Enum.filter(fn(uri) -> Euros.URI.is_same_host(uri, base_uri) end)
   end
 
   defp all_link_uris(%HTTPoison.Response{} = response) do
-    a_tags = Floki.find(response.body, "a")
-    hrefs = Floki.attribute(a_tags, "href")
     base_uri = to_base_uri(response)
-    all_uris = Enum.map(hrefs, fn(href) ->
-      Euros.Link.to_absolute(URI.parse(href), base_uri)
-    end)
-    Enum.uniq(all_uris)
+    response.body
+    |> Floki.find("a")
+    |> Floki.attribute("href")
+    |> Enum.map(fn(href) -> Euros.Link.to_absolute(URI.parse(href), base_uri) end)
+    |> Enum.filter(fn(uri) -> is_html(uri.path) end)
+    |> Enum.uniq
+  end
+
+  defp is_html(uri_path) when uri_path != nil do
+    if String.contains?(uri_path, ".") do
+      String.contains?(uri_path, ".htm") || String.contains?(uri_path, ".php")
+    else
+      true
+    end
+  end
+
+  defp is_html(uri_path) when uri_path == nil do
+    true
   end
 
   defp to_base_uri(response) do
