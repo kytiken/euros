823d20161c4d39768c42df51b41d147a816b8269
kytiken <kytiken@gmail.com>
2017-12-29 20:54:19 +0900

    Remove html files for test

--- test/euros/page_test.exs
+++ test/euros/page_test.exs
@@ -3,19 +3,23 @@ defmodule Euros.PageTest do
 
   describe "Euros.Page.link_uris/1" do
     test "get link uris from response" do
-      response = Euros.HTTP.fetch_pages("http://localhost:32768")
+      response = Euros.HTTP.fetch_pages("https://euros-test.blogspot.jp")
       links = Euros.Page.link_uris(response)
-      assert [%URI{authority: "localhost:32768", fragment: nil,
-              host: "localhost", path: "/about.html", port: 32_768,
-              query: nil, scheme: "http", userinfo: nil},
-             %URI{authority: "localhost:32768", fragment: nil,
-              host: "localhost", path: "/php.php", port: 32_768,
-              query: nil, scheme: "http", userinfo: nil},
-             %URI{authority: "localhost:32768", fragment: nil,
-              host: "localhost", path: "/nestedpath/index.html", port: 32_768,
-              query: nil, scheme: "http", userinfo: nil}] == links
-
-
+      assert [%URI{authority: "euros-test.blogspot.jp", fragment: nil,
+                host: "euros-test.blogspot.jp", path: nil, port: 443, query: nil,
+                scheme: "https", userinfo: nil},
+              %URI{authority: "euros-test.blogspot.jp", fragment: nil,
+                host: "euros-test.blogspot.jp", path: "/2017/12/image-page-test.html",
+                port: 443, query: nil, scheme: "https", userinfo: nil},
+              %URI{authority: "euros-test.blogspot.jp", fragment: nil,
+                host: "euros-test.blogspot.jp", path: "/2017/12/other-host-link-test.html",
+                port: 443, query: nil, scheme: "https", userinfo: nil},
+              %URI{authority: "euros-test.blogspot.jp", fragment: nil,
+                host: "euros-test.blogspot.jp", path: "/2017/12/test1.html", port: 443,
+                query: nil, scheme: "https", userinfo: nil},
+              %URI{authority: "euros-test.blogspot.jp", fragment: nil,
+                host: "euros-test.blogspot.jp", path: "/2017/12/", port: 443, query: nil,
+                scheme: "https", userinfo: nil}] === links
     end
   end
 end
