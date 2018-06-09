486bba8034657cdbbd8b46d243e805abfab92b73
kytiken <kytiken@gmail.com>
2018-06-09 19:02:23 +0900

    fix test

--- test/euros/core_test.exs
+++ test/euros/core_test.exs
@@ -3,7 +3,7 @@ defmodule Euros.CoreTest do
 
   describe "Euros.Core.crawl/4" do
     test "every page" do
-      url = "https://euros-test.blogspot.jp/"
+      url = "https://euros-test.blogspot.com/"
 
       assert Euros.Core.crawl(url, fn page ->
                assert 200 === page.status_code
@@ -11,7 +11,7 @@ defmodule Euros.CoreTest do
     end
 
     test "with option" do
-      url = "https://euros-test.blogspot.jp/"
+      url = "https://euros-test.blogspot.com/"
       http_option = %Euros.HTTPOption{cookie: "foo=bar;"}
       option = %Euros.CrawlOption{http_option: http_option}
 
@@ -25,7 +25,7 @@ defmodule Euros.CoreTest do
     end
 
     test "with option and url pattern" do
-      url = "https://euros-test.blogspot.jp/"
+      url = "https://euros-test.blogspot.com/"
       http_option = %Euros.HTTPOption{cookie: "foo=bar;"}
       pattern = ~r/test1/
       option = %Euros.CrawlOption{http_option: http_option, pattern: pattern}
@@ -40,7 +40,7 @@ defmodule Euros.CoreTest do
     end
 
     test "with option depth_limit is nil" do
-      url = "https://euros-test.blogspot.jp/2018/01/"
+      url = "https://euros-test.blogspot.com/2018/01/"
       option = %Euros.CrawlOption{depth_limit: nil}
 
       assert Euros.Core.crawl(
@@ -50,14 +50,14 @@ defmodule Euros.CoreTest do
 
                  assert Enum.member?(
                           [
-                            "https://euros-test.blogspot.jp/",
-                            "https://euros-test.blogspot.jp/2017/12/",
-                            "https://euros-test.blogspot.jp/2018/01/",
-                            "https://euros-test.blogspot.jp/2017/12/image-page-test.html",
-                            "https://euros-test.blogspot.jp/2017/12/test1.html",
-                            "https://euros-test.blogspot.jp/2017/12/other-host-link-test.html",
-                            "https://euros-test.blogspot.jp/2018/01/january.html",
-                            "https://euros-test.blogspot.jp/search?updated-max=2018-01-06T17:17:00%2B09:00&max-results=7"
+                            "https://euros-test.blogspot.com/",
+                            "https://euros-test.blogspot.com/2017/12/",
+                            "https://euros-test.blogspot.com/2018/01/",
+                            "https://euros-test.blogspot.com/2017/12/image-page-test.html",
+                            "https://euros-test.blogspot.com/2017/12/test1.html",
+                            "https://euros-test.blogspot.com/2017/12/other-host-link-test.html",
+                            "https://euros-test.blogspot.com/2018/01/january.html",
+                            "https://euros-test.blogspot.com/search?updated-max=2018-01-06T17:17:00%2B09:00&max-results=7"
                           ],
                           page.request_url
                         ),
@@ -68,7 +68,7 @@ defmodule Euros.CoreTest do
     end
 
     test "with option depth_limit is 0" do
-      url = "https://euros-test.blogspot.jp/2018/01/"
+      url = "https://euros-test.blogspot.com/2018/01/"
       option = %Euros.CrawlOption{depth_limit: 0}
 
       assert Euros.Core.crawl(
@@ -77,7 +77,7 @@ defmodule Euros.CoreTest do
                  assert 200 === page.status_code
 
                  assert Enum.member?(
-                          ["https://euros-test.blogspot.jp/2018/01/"],
+                          ["https://euros-test.blogspot.com/2018/01/"],
                           page.request_url
                         ),
                         page.request_url
@@ -87,7 +87,7 @@ defmodule Euros.CoreTest do
     end
 
     test "with option depth_limit is 1" do
-      url = "https://euros-test.blogspot.jp/2018/01/"
+      url = "https://euros-test.blogspot.com/2018/01/"
       option = %Euros.CrawlOption{depth_limit: 1}
 
       assert Euros.Core.crawl(
@@ -97,14 +97,14 @@ defmodule Euros.CoreTest do
 
                  assert Enum.member?(
                           [
-                            "https://euros-test.blogspot.jp/",
-                            "https://euros-test.blogspot.jp/2017/12/",
-                            "https://euros-test.blogspot.jp/2018/01/",
-                            "https://euros-test.blogspot.jp/2017/12/image-page-test.html",
-                            "https://euros-test.blogspot.jp/2017/12/test1.html",
-                            "https://euros-test.blogspot.jp/2017/12/other-host-link-test.html",
-                            "https://euros-test.blogspot.jp/2018/01/january.html",
-                            "https://euros-test.blogspot.jp/search?updated-max=2018-01-06T17:17:00%2B09:00&max-results=7"
+                            "https://euros-test.blogspot.com/",
+                            "https://euros-test.blogspot.com/2017/12/",
+                            "https://euros-test.blogspot.com/2018/01/",
+                            "https://euros-test.blogspot.com/2017/12/image-page-test.html",
+                            "https://euros-test.blogspot.com/2017/12/test1.html",
+                            "https://euros-test.blogspot.com/2017/12/other-host-link-test.html",
+                            "https://euros-test.blogspot.com/2018/01/january.html",
+                            "https://euros-test.blogspot.com/search?updated-max=2018-01-06T17:17:00%2B09:00&max-results=7"
                           ],
                           page.request_url
                         ),
